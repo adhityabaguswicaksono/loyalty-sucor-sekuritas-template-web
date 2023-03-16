35f2318df3983027edca0d819b6a92e216cadd57
adhityabaguswicaksono <48616245+adhityabaguswicaksono@users.noreply.github.com>
2023-03-16 12:31:41 +0700

    2 files modified

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        sucorblue: '#174489',
+        sucorblue: 'rgba(23, 68, 137, 1)',
+        'sucorblue-1': 'rgba(23, 68, 137, 0.25)',
+        'sucorblue-2': 'rgba(23, 68, 137, 0.75)',
       },
       maxWidth: {
         xxs: '16rem',
